6ecdf96d3dca9d094d0eadee6ad0bd9e9f2f1554
weiaicunzai <372995411@qq.com>
2018-05-10 19:48:37 +0800

    add comment

--- sort/insertion_sort.cpp
+++ sort/insertion_sort.cpp
@@ -81,6 +81,39 @@ void insertionSort2(std::vector<int> &arr)
    }
 }
 
+//similar  to  the stl implementation version
+/*
+    gcc version implementation:
+    bits/stl_algo.h
+
+  template<typename _RandomAccessIterator, typename _Compare>
+    void
+    __insertion_sort(_RandomAccessIterator __first,
+		     _RandomAccessIterator __last, _Compare __comp)
+    {
+      if (__first == __last) return;
+
+      for (_RandomAccessIterator __i = __first + 1; __i != __last; ++__i)
+	{
+    
+      //key value is less than the first element of the sorted sub array
+      //then move all the element one postion to the left
+
+	  if (__comp(__i, __first))
+	    {
+	      typename iterator_traits<_RandomAccessIterator>::value_type
+		__val = _GLIBCXX_MOVE(*__i);
+	      _GLIBCXX_MOVE_BACKWARD3(__first, __i, __i + 1);
+	      *__first = _GLIBCXX_MOVE(__val);
+	    }
+	  else
+	    std::__unguarded_linear_insert(__i,
+				__gnu_cxx::__ops::__val_comp_iter(__comp));
+	}
+}
+*/
+
+
 void insertionSort(std::vector<int> &arr)
 {
     for(int i = 0; i < arr.size(); i++) 
@@ -89,18 +122,32 @@ void insertionSort(std::vector<int> &arr)
         j = i - 1;
         key = arr[i];
 
-        while(j >=0 && key <= arr[j])
+
+        //arr[0] is the smallest
+        //in sorted array, then move
+        //all sorted array one position
+        //to the left, no other operations
+        if(key < arr[0])
         {
-            arr[j+1] = arr[j];
-            j--;
+            while(j >= 0)
+                arr[j + 1] = arr[j--];
+
+            arr[0] = key;
+            displayArr(arr);
+            continue;
         }
+
+
+        //if not, we do the regular insertion_sort
+        while(j >=0 && key <= arr[j])
+            arr[j+1] = arr[j--];
         arr[j + 1] = key;
         displayArr(arr);
     }
 }
 int main()
 {
-    int arr[] = {12, 11, 13, 5, 6, 33, 4234, 35, 66, 77};
+    int arr[] = {12, 11, 13, 5, 6, 33, 4234, 35, 66, 77, 234, 534, 53,4545,456, 5 , 6 , 87, 78};
     std::vector<int> test(arr, arr + sizeof(arr) / sizeof(int));
     //time complexity  O(n^2)
     insertionSort(test);
