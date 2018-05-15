3e1a62809e3eebf6e9e985253c1ac0aeae6eaca9
weiaicunzai <372995411@qq.com>
2018-05-15 14:38:54 +0800

    finish quick sort

--- sort/quick_sort.cpp
+++ sort/quick_sort.cpp
@@ -121,7 +121,10 @@ void quickSort1(std::vector<int> &arr)
         std::swap(arr[low++], arr[high--]);
 */
 
-// this implementation is called 
+
+
+// this parition implementation is called 
+//  Hoare Partitioning
 int partition2(std::vector<int> &arr, int low, int high)
 {
     //int key = arr[high];
@@ -148,7 +151,6 @@ void quickSort2(std::vector<int> &arr, int low, int high)
 {
     if(low >= high) return;
     int index = partition2(arr, low, high);
-    std::cout << index << std::endl;
     printArr(arr);
     quickSort2(arr, low, index - 1);
     quickSort2(arr, index, high);
@@ -159,23 +161,27 @@ void quickSort2(std::vector<int> &arr)
     quickSort2(arr, 0, arr.size() - 1);
 }
 
+
+
+//Lomuto's partition
 int partition3(std::vector<int> &arr, int low, int high) 
 {
     int pivot = arr[high];
-    int i = (low - 1);
+    int i = low - 1;
 
-    for(int j = low; j <= high; j++)
-    {
+    for(int j = low; j <= high - 1; j++)
         if(arr[j] <= pivot)
-        {
-            i++;
-        }
-    }
+            std::swap(arr[++i], arr[j]);
+
+    std::swap(arr[++i], arr[high]);
+    return i;
 }
 
 void quickSort3(std::vector<int> &arr, int low, int high)
 {
+    if(low >= high) return;
     int index = partition3(arr, low, high);
+    printArr(arr);
     quickSort3(arr, low, index - 1);
     quickSort3(arr, index + 1, high);
 
@@ -185,11 +191,45 @@ void quickSort3(std::vector<int> &arr)
     quickSort3(arr, 0, arr.size() - 1);
 }
 
-void quickSort4(std::vector<int> &arr, int low)
+
+//time complexity of quick sort:
+
+/*worst case:
+ a sorted array:
+  {5, 6,7,9,11, 23, 44, 55}
+  T(n) = T(n-1) + O(n)
+       = T(n-2) + O(n) + O(n)
+       = T(n-3) + O(n) + O(n) + O(n)
+       = ......
+       = T(1) + nO(n)
+       = O(n^2)
+
+  T(n) refer to the time complexity of quicksort
+  O(n) refer to the time complexity of paitition which is 
+  always O(n)
+*/
+
+
+/*best case:
+   everytime we pick the pivot which happens to divide the array
+   into two exactly equal parts
+
+   T(n) = 2T(n/2) + O(n)
+        = 2(2T(n/4) + (1/2) O(n)) + O(n)
+        = 4T(n/4) + 2O(n)
+        = 2^k T(n/(2^k)) + k O(n)
+
+        let T(n/(2^k)) = T(1)
+        k = log2N
+    T(n) = T(1) + logN * O(n)
+         = O(nlogN)
+*/
+
+
 int main()
 {
     int arr[] = {12, 11, 13, 5, 6, 33, 4234, 35, 66, 77, 234, 534, 53, 4545, 456, 5, 6, 87, 78};
     std::vector<int> test(arr, arr + sizeof(arr) / sizeof(int));
-    quickSort2(test);
+    quickSort3(test);
     printArr(test);
 }
